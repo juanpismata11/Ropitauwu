4c6412b541e3420162ef19889863cc8ef8791c55
Juan Pablo <juanpismata11@gmail.com>
2025-07-14 00:09:40 -0600

    reestructuracion del backend de src -> ropitauwu/backend

--- src/models/user.py
+++
@@ -1,9 +0,0 @@
-from config.database import Base
-from sqlalchemy import Column, Integer, String
-
-class User(Base):
-    __tablename__ = 'users'
-
-    id = Column(Integer, primary_key=True)
-    username = Column(String)
-    password = Column(String)
